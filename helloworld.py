5da796417d39bfbfc7198ac81e723d49d7a5b38f
Jess Pugsley <jpugsley@geekhands.com>
2021-11-23 23:24:14 -0500

    fixed file from merged

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 9574be727a846ae3aa05a7201aae9e886fd5c761
-
-
+    
 main()
